fc316623859585ad1156acbf17f19a55a9e1360f
alanguilhermeM <alanguilherme13@outlook.com>
2023-04-19 13:11:59 -0300

    Finalizado requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,23 @@
 export async function getCategories() {
   // Implemente aqui
+  try {
+    const api = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+    const categories = await api.json();
+    return categories;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  try {
+    const api = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=$QUERY');
+    const query = await api.json();
+    return query;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export async function getProductById() {
