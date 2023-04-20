130575869d8b3824afb95361473b913362ba877e
alanguilhermeM <alanguilherme13@outlook.com>
2023-04-20 14:11:07 -0300

    Finalizado requisito 9

--- src/App.js
+++ src/App.js
@@ -25,4 +25,3 @@ function App() {
 }
 
 export default App;
-// requisito 3
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
 
 export default class Cart extends Component {
+  state = {
+    produtos: [],
+  };
+
+  componentDidMount() {
+    const product = JSON.parse(localStorage.getItem('Produto'));
+    if (localStorage.getItem('Produto')) {
+      this.setState(({
+        produtos: product,
+      }));
+    }
+    // const { produtos } = this.state;
+    // console.log(produtos);
+  }
+
   render() {
+    const { produtos } = this.state;
     return (
       <section>
-        <p data-testid="shopping-cart-empty-message">
-          Seu carrinho está vazio
-        </p>
+        {produtos.length === 0 ? (
+          <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+        ) : null}
+        {produtos.length > 0 ? (
+          <>
+            {produtos.map((produto) => (
+              <div key={ Math.random() }>
+                <h3 data-testid="shopping-cart-product-name">{produto.name}</h3>
+                <img src={ produto.image } alt="Imagem" />
+                <span>{produto.price}</span>
+                <p data-testid="shopping-cart-product-quantity">
+                  {
+                    produtos.filter((product) => product.id === produto.id).length
+                  }
+                </p>
+              </div>
+            ))}
+          </>
+        ) : null}
       </section>
     );
   }
